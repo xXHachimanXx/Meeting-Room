5fadc934f52916d9eccf25766a2e8fb8d30162c7
xXHachimanXx <globaldep90@gmail.com>
2021-09-16 22:46:55 -0300

    Changing jakarta version

--- meetingroom/src/main/java/com/digital/crud/meetingroom/meetingroom/controller/RoomController.java
+++ meetingroom/src/main/java/com/digital/crud/meetingroom/meetingroom/controller/RoomController.java
@@ -5,12 +5,21 @@ import com.digital.crud.meetingroom.meetingroom.model.Room;
 import com.digital.crud.meetingroom.meetingroom.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
